61a9bb54bbf82a44d8619c219870cbafcac3acdb
Apurvachavan <apurvachavan44@gmail.com>
2023-02-07 11:16:37 +0530

    a

---
+++ examapp/src/main/java/com/app/pojos/BaseEntity.java
@@ -0,0 +1,45 @@
+package com.app.pojos;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@MappedSuperclass // mandatory cls level anno to tell hibernate that following is a common base
+					// class to extend entities , no table generation
+//@Getter
+//@Setter
+//@ToString
+public class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BaseEntity(Long id) {
+		super();
+		this.id = id;
+	}
+
+	public BaseEntity() {
+		super();
+	}
+
+	@Override
+	public String toString() {
+		return "BaseEntity [id=" + id + "]";
+	}
+	
+	
+}
